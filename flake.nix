69efc73268773a2d9b74adca96a5eb902a576578
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-02-28 21:29:47 +0100

    add nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,15 +2,18 @@
   description =
     "A minimal & fast plugin loader for nixified neovim configurations";
 
-  inputs.utils.url = "github:NewDawn0/nixUtils";
+  inputs = {
+    utils.url = "github:NewDawn0/nixUtils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
 
-  outputs = { self, utils }: {
+  outputs = { self, nixpkgs, utils }: {
     overlays.default = final: prev: {
       vimPlugins = prev.vimPlugins // {
         loader-nvim = self.packages.${prev.system}.default;
       };
     };
-    packages = utils.lib.eachSystem { } (pkgs: {
+    packages = utils.lib.eachSystem { inherit nixpkgs; } (pkgs: {
       default = pkgs.vimUtils.buildVimPlugin {
         name = "loader-nvim";
         src = ./.;
