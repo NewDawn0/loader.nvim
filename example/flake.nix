952f325f9af454517326aef752d20713a09396f1
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-23 16:32:01 +1000

    update example to new utils version

--- example/flake.nix
+++ example/flake.nix
@@ -2,28 +2,19 @@
   description = "NewDawn0's nixified nvim config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    utils = {
-      url = "github:NewDawn0/nixUtils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    loader = {
+    utils.url = "github:NewDawn0/nixUtils";
+    loader-nvim = {
       url = "github:NewDawn0/loader.nvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
     };
   };
-  outputs = { nixpkgs, utils, ... }@inputs:
-    let
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ inputs.loader.overlays.default ];
-        };
-    in {
-      # utils.lib.eachSystem just loops over all the systems providing the correct packages
-      # Here we use a custom mkPkgs function with an overlay which always exposing the loader.nvim plugin
-      packages = utils.lib.eachSystem { inherit mkPkgs; } (pkgs:
-        let nvim = import ./nvim.nix { inherit pkgs; };
-        in { default = nvim; });
-    };
+  outputs = { utils, ... }@inputs: {
+    # utils.lib.eachSystem just loops over all the systems providing the correct packages
+    # Here we use a custom mkPkgs function with an overlay which always exposing the loader.nvim plugin
+    packages = utils.lib.eachSystem {
+      overlays = [ inputs.loader-nvim.overlays.default ];
+    } (pkgs:
+      let nvim = import ./nvim.nix { inherit pkgs; };
+      in { default = nvim; });
+  };
 }
